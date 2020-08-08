d47c9bcb6745a0a372de858025ceaed52ab237cf
Haritha <v.haritha11197@gmail.com>
2020-08-08 14:59:07 +0530

    Redbus-booking project

---
+++ src/main/java/Red-bus/App.java
@@ -0,0 +1,13 @@
+package Red-bus;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
